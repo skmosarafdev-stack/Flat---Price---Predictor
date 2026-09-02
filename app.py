86702f1dff5877a90c9e9792c5f157bb0d9413ab
skmosarafdev-stack <skmosaraf.dev@gmail.com>
2026-09-02 21:52:05 +0530

    Update model path to 'mode1/model.pkl'

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 app = Flask(__name__)
 
 # Model load
-with open('model.pkl', 'rb') as f:
+with open('mode1/model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 @app.route('/', methods=['GET', 'POST'])
